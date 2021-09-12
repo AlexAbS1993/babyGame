78bf3093cf67e506a122acd80edb50f266b9fb61
Alex Abetov <abetovalex93@gmail.com>
2021-09-12 20:31:25 +0300

    Добавлен класс к файлу config
    1) Создана база для изменения config в формат класса
    2) Создан класс для создания объекта. В классе имеются поля, но пока нет
       методов

--- src/config/config.ts
+++ src/config/config.ts
@@ -32,3 +32,44 @@ export const config = {
         }
     }
 }
+
+class Config {
+    public general
+    public animalGame
+    public wordGame
+    public Settings
+    public delays
+    constructor() {
+        this.general = {
+            devMode: true
+        }
+        // ==== КОНСТАНТЫ ДЛЯ ИГРЫ В ЖИВОТНЫХ ==== //
+        this.animalGame = {
+            countOfAnimals: 6,
+            __rows: 2,
+            rounds: 2,
+            __stages: ["presentation", "quiz"]
+        }
+        // ==== КОНСТАНТЫ ДЛЯ ИГРЫ В БУКВЫ ==== //
+        this.wordGame = {
+            rounds: 6,
+            wordsPortion: 3,
+            __wordsCount: 33,
+            chooseVariants: 3,
+            __stages: ["presentation", "quiz"],
+            __wordsCountPresentation: 2,
+            __potentialWordsParties: 7 // Должно быть 11
+        }
+        this.Settings = {
+            musicMuted: false,
+            volume: 0.1
+        }
+        this.delays = {
+            AnimalGame: {
+                presentationEndOfStage: 1100,
+                presentatitonEnableAllButtons: 500,
+                quizSetWin: 700
+            }
+        }
+    }
+}
