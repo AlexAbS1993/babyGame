365068cb9b1ac44349c9cb50549ce679a0bc87d0
Alex Abetov <abetovalex93@gmail.com>
2021-09-13 18:54:53 +0300

    Добавлен тестовый файл для канваса

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,5 +1,21 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import classes from './Test.module.css'
 
 export const Test = () => {
-  return <></>
+  const [initializeTest, setInitializeTest] = useState(false)
+  useEffect(() => {
+    setInitializeTest(true)
+  }, [])
+  return (
+    <>
+      {initializeTest && (
+        <div className={classes.wrapper}>
+          <div className={classes.side}></div>
+          <div className={classes.canvas}>
+            <canvas className={classes.canvas__holst}></canvas>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
